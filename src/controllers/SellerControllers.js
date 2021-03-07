406721386a45cc07890f90b1d6f87f9d3b096f07
nick2905 <nicolasmanurung16@gmail.com>
2021-03-07 18:43:59 +0700

    fix voucher query

--- src/controllers/SellerControllers.js
+++ src/controllers/SellerControllers.js
@@ -898,10 +898,10 @@ export const postOneVoucher = async(req, res) => {
 export const getVoucherBySeller = async(req, res) => {
     try {
         const findAllVoucher = await Voucher.find({
-            idSellerAccount: req.params.idSellerAccount,
-            validity: {
-                $gt: new Date(Date.now())
-            }
+            idSellerAccount: req.params.idSellerAccount
+                // validity: {
+                //     $gt: new Date(Date.now())
+                // }
         });
         return res.status(200).json({
             success: true,
